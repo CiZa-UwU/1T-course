463b1250f5a6fe2889026834233491e9dc45e1f8
CiZa-UwU <oleg.chernyshov.960@mail.ru>
2023-03-08 12:45:24 +0300

    Static todos

--- Quasar-ToDo-list/Quasar-ToDo-list/src/pages/Todo.vue
+++ Quasar-ToDo-list/Quasar-ToDo-list/src/pages/Todo.vue
@@ -1,12 +1,41 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Logo"
-      src="~assets/microsoft-todo-svgrepo-com.svg"
-      style="width:200px; height: 200px;"
-    >
+  <q-page class="column bg-grey-3">
+    <q-list
+    separator
+    bordered>
+      <q-item 
+      v-for="task in tasks" :key="task.title"
+      tag="label" v-ripple>
+        <q-item-section avatar>
+          <q-checkbox v-model="task.done" val="teal" color="teal" />
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>{{ task.title }}</q-item-label>
+        </q-item-section>
+      </q-item>
+    </q-list>
   </q-page>
 </template>
 
 <script>
+export default{
+  data(){
+    return{
+      tasks: [
+        {
+          title: 'Hello vue',
+          done: false
+        },
+        {
+          title: 'Hello world',
+          done: false
+        },
+        {
+          title: '123',
+          done: false
+        }
+      ]
+    }
+  }
+}
 </script>
